3c9c8100db64383f08b6f0834a95d4f91742f049
Fedora <singhraoharshit3@gmail.com>
2026-03-28 14:10:52 +0000

    show gitignored files in nvim-tree

--- init.lua
+++ init.lua
@@ -119,7 +119,11 @@ require("lazy").setup({
         "nvim-tree/nvim-tree.lua",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
-            require("nvim-tree").setup()
+            require("nvim-tree").setup({
+                git = {
+                    ignore = false,
+                },
+            })
         end,
     },
 
